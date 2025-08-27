4cda3e4ebfdddb72e12169e2be190c41ace93037
Yang Wujie <wujieyang@gmail.com>
2025-08-27 17:23:58 +0800

    fix zicsr error

--- 01-HelloOs/os.c
+++ 01-HelloOs/os.c
@@ -3,7 +3,7 @@
 #define UART        0x10000000
 #define UART_THR    (uint8_t*)(UART+0x00) // THR:transmitter holding register
 #define UART_LSR    (uint8_t*)(UART+0x05) // LSR:line status register
-#define UART_LSR_EMPTY_MASK 0x40          // LSR Bit 6: Transmitter empty; both the THR and LSR are empty
+#define UART_LSR_EMPTY_MASK 0x40          // LSR Bit 6: Transmitter empty
 
 int lib_putc(char ch) {
 	while ((*UART_LSR & UART_LSR_EMPTY_MASK) == 0);
